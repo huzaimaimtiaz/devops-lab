ec44ef7c8c4465639e2fad0802177889080774d7
huzaimaimtiaz <168641748+huzaimaimtiaz@users.noreply.github.com>
2026-09-23 13:32:44 +0500

    Create HelloHuzaima.java

---
+++ HelloHuzaima.java
@@ -0,0 +1,10 @@
+public class HelloHuzaima {
+    public static void main(String[] args) {
+        String name = "Huzaima";
+        String rollNo = "FA23-BDS-017";
+
+        System.out.println("Hello, World!");
+        System.out.println("Name: " + name);
+        System.out.println("Roll No: " + rollNo);
+    }
+}
